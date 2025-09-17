967355d6051dd0130e9f39c221ba994d3cd4f8f5
jmplatt <122328869+jmplatt@users.noreply.github.com>
2025-09-17 16:57:12 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -169,7 +169,7 @@ io.on('connection', socket => {
 // Serve React build folder in production
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
