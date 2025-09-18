e28af604bb86dd9744b2c8c5868da2e0eddf7804
jmplatt <122328869+jmplatt@users.noreply.github.com>
2025-09-17 17:17:13 -0700

    Update YahtzeeGame.js

--- client/src/YahtzeeGame.js
+++ client/src/YahtzeeGame.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001"); // backend URL
+const socket = io();
 
 export default function YahtzeeGame() {
   const [gameId, setGameId] = useState("");
